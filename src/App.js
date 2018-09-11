9670c6a59044d590909f63ad5ea801516f440756
armved <avetik.harutyunyan1@gmail.com>
2018-09-11 21:42:39 +0300

    Build initial project structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Root from './components/Root';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Root></Root>
+      </div>
+    );
+  }
+}
+
+export default App;
