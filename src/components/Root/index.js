d268dbaec7d37cc13fdfd82b6800b6655b6fb6ac
armved <avetik.harutyunyan1@gmail.com>
2018-09-11 23:03:46 +0300

    Use fullpageJS for sections

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import ReactFullpage from '@fullpage/react-fullpage';
+import './styles.css';
 
 const Root = () => (
-  <h1>Hello World</h1>
+  <ReactFullpage
+    licenseKey='OPEN-SOURCE-GPLV3-LICENSE'
+    anchors={['firstPage', 'secondPage', 'thirdPage']}
+    sectionsColor={['#6ed3cf', '#9068be', '#e62739']}
+    paddingTop='0px'
+    navigation={true}
+    arrowNavigation={true}
+    render={({ state, fullpageApi }) => {
+      return (
+        <div>
+          <div className="section general-info">
+            <div className="general-info-wrapper">
+              <img className="avatar" src="avatar.png"></img>
+              <span className="name">Avetik Harutyunyan</span>
+              <span className="title">Frontend Developer</span>
+            </div>
+          </div>
+
+          <div className="section">
+            <p>Section 2</p>
+          </div>
+
+          <div className="section">
+            <p>Section 2</p>
+          </div>
+        </div>
+      );
+    }}
+  />
 );
 
 export default Root;
